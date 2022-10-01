b2c4133ae0216dee57243835d4b9d99480c609bc
GArifullinII <ildaridrisovich@gmail.com>
2022-10-01 12:00:29 +0400

    Initial Commit

---
+++ swiftUI_calculator/swiftUI_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_calculatorApp.swift
+//  swiftUI_calculator
+//
+//  Created by Ildar Garifullin on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
